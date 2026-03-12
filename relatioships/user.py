a57ac8fdfd8a65410a8400471fc8e320c3073cfa
Meaow-09 <Meaow-756103297@outlook.com>
2026-03-12 18:03:10 +0100

    03/12 18:00

--- relatioships/user.py
+++ relatioships/user.py
@@ -1,12 +1,27 @@
-class User:
-    def __init__(self, id: int, name: str, group: int):
-        self._id = id
+class Book:
+    def __init__(self, bID: int, name: str):
+        self._bID = bID
         self._name = name
-        self._group = group
 
     @property
-    def id(self):
-        return self._id
+    def bID(self):
+        return self._bID
+
+    @property
+    def name(self):
+        return self._name
+
+
+class User:
+    def __init__(self, uID: int, name: str, group: int):
+        self._uID = uID
+        self._name = name
+        self._group = group
+        self._books = []
+
+    @property
+    def uID(self):
+        return self._uID
 
     @property
     def name(self):
@@ -16,34 +31,80 @@ class User:
     def group(self):
         return self._group
 
+    @property
+    def books(self):
+        return self._books
+
     def __str__(self):
-        return "Member: " + self._name + ", ID: " + str(self._id) + ", Group: " + str(self._group)
+        return "Member: " + self.name + ", ID: " + str(self.uID) + ", Group: " + str(self.group)
 
     def scan(self):
-        print("**", self._id, "is scanning. **")
-        pass
+        print("**", self.uID, "is scanning. **")
 
     def borrowBook(self, bID: int):
-        print(self._id, "is borrowing", bID)
-        pass
+        print(self.uID, "is borrowing", bID)
+        if len(self.books) <= 3:
+            self.books.append(bID)
+            print("** Successfully borrowed", bID, "**")
+        else:
+            print("!! Failed to borrow", bID, ", you can only borrow 3 books at a time. !!")
 
     def returnBook(self, bID: int):
-        print(self._id, " s returning", bID)
-        pass
+        print(self.uID, " s returning", bID)
+        if bID in self._books:
+            self.books.remove(bID)
+            print("** Successfully returned", bID, "**")
+        else:
+            print("!! Failed to return", bID, ", please check your borrow list. !!")
 
 
 class Librarian(User):
-    def __init__(self, empId: int, name: str, group: int):
-        self.super(empId, name, group)
+    def __init__(self, empID: int, name: str):
+        super().__init__(empID, name, 0)
 
     @property
-    def empId(self):
-        return self._id
+    def empID(self):
+        return self._uID
 
+    def __str__(self):
+        return "# Librarian: " + self._name + ", ID: " + str(self.empID) + ", Group: " + str(self.group)
+
+    def manageUser(self, uID, action):
+        print("**", self.empID, "is", action, uID, "**")
+
+    def manageBook(self, bID, action):
+        print("**", self.empID, "is", action, bID, "**")
+
+    def changeOverdueBook(self, bID, uID):
+        pass
+
+
+class Read:
+    def __init__(self, user: User, books: list):
+        self._user = user
+        self._books = books
+
+    @property
+    def uID(self):
+        return self._user.uID
+
+    @property
+    def books(self):
+        return self._books
+
+    @property
+    def booksCount(self):
+        return len(self._books)
 
 
 if __name__ == "__main__":
-    user = User(1, "Mimi", 9)
-    print(user)
-    user.scan()
-    user.borrowBook(10086)
+    mimi = User(1001, "Mimi", 9)
+    print(mimi)
+    mimi.scan()
+    mimi.borrowBook(10086)
+    print(mimi.books)
+    mimi.returnBook(10086)
+
+    admin = Librarian(0, "Admin")
+    print(admin)
+
