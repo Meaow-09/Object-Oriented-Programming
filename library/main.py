40b16201cbd25121eb2abc4adbc4b1c55080eb81
Meaow-09 <Meaow-756103297@outlook.com>
2026-03-16 18:35:48 +0100

    save user as pkl file

--- library/main.py
+++ library/main.py
@@ -1,3 +1,5 @@
+import pickle
+
 from library.book import Book
 from library.user import User
 from library.librarian import Librarian
@@ -17,34 +19,44 @@ def tryReturnBook(user: User, book):
         print(err)
 
 
-mimi = User(1001, "Mimi", 9)
-print(mimi)
-mimi.scan()
+if __name__ == "__main__":
+    mimi = User(1001, "Mimi", 9)
+    print(mimi)
+    mimi.scan()
 
-ba = Book(901, "A book")
-bb = Book(902, "B book")
-bc = Book(903, "C book")
-bd = Book(904, "D book")
+    ### try to load the user from the serialized obj
+    with open("./mimi.pkl", mode="rb") as file:
+        user1 = pickle.load(file)
 
-tryBorrowBook(mimi, ba)
-tryBorrowBook(mimi, bb)
-tryBorrowBook(mimi, bb)
+    ### serialize the user
+    with open("./mimi.pkl", mode="wb") as file:
+        pickle.dump(user1, file)
+    user1.scan()
 
-tryReturnBook(mimi, bc)
-tryBorrowBook(mimi, bc)
-tryBorrowBook(mimi, bd)
+    ba = Book(901, "A book")
+    bb = Book(902, "B book")
+    bc = Book(903, "C book")
+    bd = Book(904, "D book")
 
-mimi.showBooks()
+    tryBorrowBook(mimi, ba)
+    tryBorrowBook(mimi, bb)
+    tryBorrowBook(mimi, bb)
 
-try:
-    me = User(10011, "me", 1)
-except Exception as err:
-    print(err)
+    tryReturnBook(mimi, bc)
+    tryBorrowBook(mimi, bc)
+    tryBorrowBook(mimi, bd)
 
-# admin = Librarian(0, "Admin")
-# print(admin)
-# admin.lendBooks(mimi, [902, 903, 904])
-# print(mimi.books)
+    mimi.showBooks()
 
-# lend books to read
-# print each users' reading books
+    try:
+        me = User(10011, "me", 1)
+    except Exception as err:
+        print(err)
+
+    # admin = Librarian(0, "Admin")
+    # print(admin)
+    # admin.lendBooks(mimi, [902, 903, 904])
+    # print(mimi.books)
+
+    # lend books to read
+    # print each users' reading books
